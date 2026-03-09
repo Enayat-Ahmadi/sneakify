981e812f29d5309cc0e1b53e35ed57054fa76a4b
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2026-03-09 17:18:02 +0100

    install shadcn and implement GET method

---
+++ pages/api/products.js
@@ -0,0 +1,20 @@
+import dbConnect from "@/db/connect";
+import Product from "@/db/models/Product";
+
+const handler = async (req, res) => {
+  await dbConnect();
+  if (req.method === "GET") {
+    try {
+      const products = Product.find();
+      res.status(200).json(products);
+      return;
+    } catch (error) {
+      res.status(404).json(error.message);
+      return;
+    }
+  }
+  res.status(405).json({ status: "Method not allowed" });
+  return;
+};
+
+export default handler;
