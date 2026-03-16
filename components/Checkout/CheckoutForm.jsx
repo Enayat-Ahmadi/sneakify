078ef72be3ebdba825c0ab1035f377243d8db806
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2026-03-16 14:59:38 +0100

    add address field to checkout form:

--- components/Checkout/CheckoutForm.jsx
+++ components/Checkout/CheckoutForm.jsx
@@ -2,6 +2,7 @@ import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { Card, CardTitle, CardHeader, CardContent } from "../ui/card";
+import { Field } from "../ui/field";
 export default function CheckoutForm({ onSubmit }) {
   return (
     <div className="mx-auto min-h-screen grid max-w-7xl gap-6 lg:grid-cols-3">
@@ -11,7 +12,7 @@ export default function CheckoutForm({ onSubmit }) {
             <CardTitle>Customer Information</CardTitle>
           </CardHeader>
           <CardContent className="grid gap-4 md:grid-cols-2">
-            <div className="space-y-2 md:col-span-2">
+            <Field className="space-y-2 md:col-span-2">
               <Label htmlFor="fullname">Full Name</Label>
               <Input
                 type="text"
@@ -20,8 +21,8 @@ export default function CheckoutForm({ onSubmit }) {
                 placeholder="John Doe"
                 required
               />
-            </div>
-            <div className="space-y-2">
+            </Field>
+            <Field className="space-y-2">
               <Label htmlFor="email">Email</Label>
               <Input
                 type="email"
@@ -30,8 +31,8 @@ export default function CheckoutForm({ onSubmit }) {
                 placeholder="email@email.com"
                 required
               />
-            </div>
-            <div className="space-y-2">
+            </Field>
+            <Field className="space-y-2">
               <Label htmlFor="phone">Phone</Label>
               <Input
                 type="phone"
@@ -39,6 +40,45 @@ export default function CheckoutForm({ onSubmit }) {
                 placeholder="+49 123 456789"
                 required
               />
+            </Field>
+          </CardContent>
+        </Card>
+        <Card className="rounded-2xl">
+          <CardHeader>
+            <CardTitle>Shiping Address</CardTitle>
+          </CardHeader>
+          <CardContent className="grid gap-4 md:col-span-2">
+            <Field className="space-y-2">
+              <Label htmlFor="street">Street</Label>
+              <Input
+                type="text"
+                name="street"
+                id="street"
+                placeholder="Street name and house number"
+                required
+              />
+            </Field>
+            <div className="grid grid-cols-2 gap-4">
+              <Field className="space-y-2">
+                <Label htmlFor="city">City</Label>
+                <Input
+                  type="text"
+                  name="city"
+                  id="city"
+                  placeholder="Berlin"
+                  required
+                />
+              </Field>
+              <Field className="space-y-2">
+                <Label htmlFor="postalCode">Postal Code</Label>
+                <Input
+                  type="number"
+                  name="postalCode"
+                  id="postalCode"
+                  placeholder="10233"
+                  required
+                />
+              </Field>
             </div>
           </CardContent>
         </Card>
