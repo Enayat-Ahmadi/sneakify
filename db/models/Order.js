85afbed0be5aa768b1966c08780f2e8386e38ac3
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2026-03-18 11:46:15 +0100

    fix orderSchema bug

--- db/models/Order.js
+++ db/models/Order.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose;
 const orderSchema = new Schema(
   {
     customer: {
-      fullname: { type: String, required: true },
+      fullName: { type: String, required: true },
       email: { type: String, required: true },
       address: { type: String, required: true },
       city: { type: String, required: true },
@@ -17,7 +17,7 @@ const orderSchema = new Schema(
           ref: "Product",
           required: true,
         },
-        name: { type: string, required: true },
+        name: { type: String, required: true },
         price: { type: Number, required: true },
         quantity: { type: Number, required: true },
       },
