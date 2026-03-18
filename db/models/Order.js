c0b23e1e5648b547c886fe2fbd26832abda00979
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2026-03-18 08:51:38 +0100

    create order schema :

---
+++ db/models/Order.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const orderSchema = new schema(
+  {
+    customer: {
+      fullname: { type: String, required: true },
+      email: { type: String, required: true },
+      address: { type: String, required: true },
+      city: { type: String, required: true },
+      postalCode: { type: Number, required: true },
+    },
+    items: [
+      {
+        productId: {
+          type: Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        name: { type: string, required: true },
+        price: { type: Number, required: true },
+        quantity: { type: Number, required: true },
+      },
+    ],
+    totalAmount: { type: Number, required: true },
+    status: {
+      type: String,
+      default: "pending",
+    },
+  },
+  { tiemstamps: true },
+);
+const Order = models.Order || model("Order", orderSchema);
+export default Order;
