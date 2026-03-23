da679aaca10c03fb14e5970338b7f768af28f577
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2026-03-23 10:33:14 +0100

    add searchForm component:

---
+++ components/SearchForm.jsx
@@ -0,0 +1,26 @@
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+
+export default function SearchForm() {
+  const router = useRouter();
+  const [search, setSearch] = useState(router.query.search || "");
+
+ 
+  return (
+    <form onSubmit={handleSerach} className="flex">
+      <Input
+        type="text"
+        name="search"
+        id="search"
+        placeholder="search product..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <Button type="submit" variant="fhost">
+        search
+      </Button>
+    </form>
+  );
+}
