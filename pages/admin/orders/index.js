5fbbf0b61871441913f412b58d71a66b4f75051e
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2026-03-25 18:20:04 +0100

    inport adminorderCard to order page

--- pages/admin/orders/index.js
+++ pages/admin/orders/index.js
@@ -1,7 +1,7 @@
 import useSWR from "swr";
 import ErrorScreen from "@/components/ui/ErrorScreen";
 import LoadingScreen from "@/components/ui/LoadingScreen";
-
+import AdminOrderCard from "@/components/Admin/AdminOrderCard";
 
 export default function Orders() {
   const { data, error, isLoading } = useSWR("/api/orders");
@@ -11,7 +11,9 @@ export default function Orders() {
   const orders = Array.isArray(data) ? data : [];
   return (
     <main className="max-w-6xl min-h-screen flex flex-col mx-auto gap-3 bg-background px-4 py-8 md:px-8">
-
+      {orders.map((order) => (
+        <AdminOrderCard key={order._id} order={order} />
+      ))}
     </main>
   );
 }
