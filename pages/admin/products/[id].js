1447531856fd425ea7390b6e77dc37ac5feb135d
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2026-03-26 15:02:42 +0100

    add loading to update form

--- pages/admin/products/[id].js
+++ pages/admin/products/[id].js
@@ -3,8 +3,10 @@ import useSWR from "swr";
 import ProductForm from "@/components/Admin/ProductForm";
 import ErrorScreen from "@/components/ui/ErrorScreen";
 import LoadingScreen from "@/components/ui/LoadingScreen";
+import { useState } from "react";
 
 export default function AdminProductDetailsPage() {
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
   const { id } = router.query;
   const {
@@ -17,6 +19,7 @@ export default function AdminProductDetailsPage() {
   if (isLoading) return <LoadingScreen />;
 
   async function handleUpdate(productData) {
+    setLoading(true);
     const response = await fetch(`/api/products/${id}`, {
       method: "PUT",
       headers: {
@@ -25,6 +28,7 @@ export default function AdminProductDetailsPage() {
       body: JSON.stringify(productData),
     });
     const data = await response.json();
+    setLoading(false);
     if (!response.ok) {
       throw new Error(data.message || "Failed to update product");
     }
@@ -33,12 +37,12 @@ export default function AdminProductDetailsPage() {
     router.push("/admin/products");
   }
   return (
-    <main>
-      Product id :{id}
+    <main className="px-4 py-8 md:px-8">
       <ProductForm
         onSubmit={handleUpdate}
         initialData={product}
         isEditing="true"
+        loading={loading}
       />
     </main>
   );
