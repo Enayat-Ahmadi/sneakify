b175cf1862d0d93aeb66f6adb3834ffa13b23061
Enayat Ahmadi <eno.ahmadi22@gmail.com>
2026-03-26 14:51:33 +0100

    feat(admin):implement update product logic and refresh product data

--- pages/admin/products/[id].js
+++ pages/admin/products/[id].js
@@ -1,11 +1,41 @@
 import { useRouter } from "next/router";
 import useSWR from "swr";
+import ProductForm from "@/components/Admin/ProductForm";
 
 export default function AdminProductDetailsPage() {
   const router = useRouter();
   const { id } = router.query;
-  const { data: product, error } = useSWR(`/api/products/${id}`);
-  console.log(product);
+  const {
+    data: product,
+    error,
+    isLoading,
+    mutate,
+  } = useSWR(`/api/products/${id}`);
 
-  return <main>Product id :{id}</main>;
+  async function handleUpdate(productData) {
+    const response = await fetch(`/api/products/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productData),
+    });
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error(data.message || "Failed to update product");
+    }
+    await mutate(`/api/products/${id}`);
+    await mutate("/api/products");
+    router.push("/admin/products");
+  }
+  return (
+    <main>
+      Product id :{id}
+      <ProductForm
+        onSubmit={handleUpdate}
+        initialData={product}
+        isEditing="true"
+      />
+    </main>
+  );
 }
